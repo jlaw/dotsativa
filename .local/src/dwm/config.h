915f6b0ace15d53e66a51bb87d5e19ddc2285ffc
Jackson Law <178053+jlaw@users.noreply.github.com>
2020-08-14 09:42:43 -0700

    chore: Personalize dwm
    - Apply tag 9 to SmartGit
    - Apply tag 8 to Slack
    - Change tag labels to Chinese numerals
    - Reduce outer vertical gap

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -7,10 +7,10 @@
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 30;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -42,7 +42,8 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+static const char *tags[] = { "〡", "〢", "〣", "〤", "〥", "〦", "〧", "〨", "〩" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -50,7 +51,8 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title           tags mask     isfloating   isterminal noswallow monitor */
-	{ "Gimp",     NULL,       NULL,           1 << 8,       0,           0,         0,        -1 },
+	{ "Slack",    NULL,       NULL,           1 << 7,       0,           0,         0,        -1 },
+	{ "SmartGit", NULL,       NULL,           1 << 8,       0,           0,         0,        -1 },
 	{ TERMCLASS,  NULL,       NULL,           0,            0,           1,         0,        -1 },
 	{ NULL,       NULL,       "Event Tester", 0,            0,           0,         1,        -1 },
 	{ NULL,      "spterm",    NULL,           SPTAG(0),     1,           1,         0,        -1 },
